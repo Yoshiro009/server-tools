cf4962b3682d4529103d4d8c7e46af91f0e4725c
Katherine Zaoral <kz@adhoc.com.ar>
2018-07-25 18:08:04 -0300

    [IMP] base_name_search_improved: Add Smart Search

--- base_name_search_improved/__manifest__.py
+++ base_name_search_improved/__manifest__.py
@@ -3,10 +3,10 @@
 {
     'name': 'Improved Name Search',
     'summary': 'Friendlier search when typing in relation fields',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Uncategorized',
     'website': 'https://odoo-community.org/',
-    'author': 'Daniel Reis, Odoo Community Association (OCA)',
+    'author': 'Daniel Reis, Odoo Community Association (OCA), ADHOC SA',
     'license': 'AGPL-3',
     'data': [
         'views/ir_model_views.xml',
